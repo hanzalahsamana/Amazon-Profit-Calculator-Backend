a8f03e4057676c8b239511c99f204abca48bd67d
hanzalahsamana <hanzalahsamana789@gmail.com>
2025-08-09 16:35:18 +0500

    Export App

--- server.js
+++ server.js
@@ -1,12 +1,25 @@
 const express = require("express");
 const cors = require("cors");
+require("dotenv").config();
 const mainRouter = require("./routes/index");
+const bodyParser = require("body-parser");
 
 const app = express();
 
-app.use(cors()); // Allow all origins
-app.use(express.json());
+const corsOptions = {
+  origin: "*",
+  methods: ["GET", "POST", "PUT", "DELETE", "PATCH", "OPTIONS"],
+  allowedHeaders: ["Content-Type", "Authorization"],
+  credentials: true,
+  preflightContinue: false,
+  optionsSuccessStatus: 204,
+};
 
+app.use(cors(corsOptions));
+
+
+app.options("*", cors(corsOptions));
+app.use(bodyParser.json());
 
 app.use("/api/v1", mainRouter);
 
